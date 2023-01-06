4d0cd13da9893367bdd6c39f7473444483e3330d
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-01-06 16:10:01 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
-  console.log(`App Listen to http://${process.env.URL}:${process.env.PORT}`);
+  console.log(`App Listen to Port ${process.env.PORT}`);
 }
 bootstrap();
